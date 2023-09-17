138ae966963f2e8361ad2abe7e113c97004a205d
MariaMolchanova <145238309+MariaMolchanova@users.noreply.github.com>
2023-09-17 13:57:34 +1200

    Update streamlit_app.py
    added emogy

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menue')
-streamlit.text('Omega 3 a& Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('hard-Boild Free-Range Egg')
+streamlit.text('🥣Omega 3 a& Blueberry oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boild Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
