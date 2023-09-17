aadacc8dd999232ead2dcf7c219533ec2df4a5cf
MariaMolchanova <145238309+MariaMolchanova@users.noreply.github.com>
2023-09-17 13:54:37 +1200

    Update streamlit_app.py
    added Header
    added menue Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menue')
+streamlit.text('Omega 3 a& Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('hard-Boild Free-Range Egg')
